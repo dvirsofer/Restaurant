55474cfe19a4891859869851ae8b469a23559718
dvirsofer <dvir.sofer90@gmail.com>
2015-06-29 06:19:00 -0700

    add main view

---
+++ DanRestaurantApp/Classes/Controllers/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  DanRestaurantApp
+//
+//  Created by DvirSofer on 6/29/15.
+//  Copyright (c) 2015 Or. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+-(IBAction)exitButtonAction:(id)sender;
+-(IBAction)historyButtonAction:(id)sender;
+-(IBAction)cartButtonAction:(id)sender;
+
+@end
---
+++ DanRestaurantApp/Classes/Controllers/MainViewController.m
@@ -0,0 +1,47 @@
+//
+//  MainViewController.m
+//  DanRestaurantApp
+//
+//  Created by DvirSofer on 6/29/15.
+//  Copyright (c) 2015 Or. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIBarButtonItem *historyButton = [[UIBarButtonItem alloc] initWithTitle:@"היסטוריה" style:UIBarButtonItemStylePlain target:self action:@selector(historyButtonAction:)];
+    UIBarButtonItem *cartButton = [[UIBarButtonItem alloc] initWithTitle:@"עגלה" style:UIBarButtonItemStylePlain target:self action:@selector(cartButtonAction:)];
+    
+    NSArray *navButtonArray = [[NSArray alloc] initWithObjects:historyButton, cartButton, nil];
+    self.navigationItem.rightBarButtonItems = navButtonArray;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)exitButtonAction:(id)sender {
+    // When clicked exit
+    NSLog(@"Exit clicked");
+}
+
+-(void)historyButtonAction:(id)sender {
+    // When clicked history
+    NSLog(@"History clicked");
+}
+
+-(void)cartButtonAction:(id)sender {
+    // When clicked cart
+    NSLog(@"Cart clicked");
+}
+
+@end
