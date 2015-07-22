62598e04c582a2b24d9ebdcd197e2850642a5720
ormed <ormed1994@gmail.com>
2015-07-22 05:22:15 -0700

    Authorization Edited

--- DanRestaurantApp/Classes/Models/Authorization+CoreData.h
+++ DanRestaurantApp/Classes/Models/Authorization+CoreData.h
@@ -3,7 +3,7 @@
 //  DanRestaurantApp
 //
 //  Created by Or on 7/22/15.
-//  Copyright (c) 2015 Or. All rights reserved.
+//  Copyright (c) 2015 Or & Dvir. All rights reserved.
 //
 
 #import "Authorization.h"
